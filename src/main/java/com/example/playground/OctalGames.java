e9903a3a85f827ed795731dbeec554cf1da64e99
1fa07JG <1fao7-gfi-2020@mail.de>
2022-02-07 16:03:51 +0100

    Add class to playground

---
+++ src/main/java/com/example/playground/OctalGames.java
@@ -0,0 +1,21 @@
+package com.example.playground;
+
+public class OctalGames {
+
+
+    public static void main(String[] args) {
+
+        // hexadecimal (16)
+        // decimal (10)
+        // octal (8)
+        // binär (2)
+
+        int x = 0x12;  // decimal = 18
+        int y = 0b011; // decimal = 3
+        int z = 0100; // octal "10" = 8 + 0 = decimal 10
+
+        System.out.println("octal " + z);
+
+
+    }
+}
